c7ca510d0b0e90895d3863ad0ab1d6aadd5111b2
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-26 10:38:44 +0530

    action type

--- Redux-demo.js
+++ Redux-demo.js
@@ -1,8 +1,26 @@
 const redux=require('redux');
 const counterReducer=(state={counter:0},action)=>{
-    return {
+   if(action.type==='increment')
+   { return {
         counter:state.counter+1,
     }
+}
+if(action.type==='decrement')
+{ return {
+     counter:state.counter-1,
+ }
+}
+if(action.type==='incrementby2')
+{ return {
+     counter:state.counter+2,
+ }
+}
+if(action.type==='decrementby2')
+{ return {
+     counter:state.counter-2,
+ }
+}
+return state;
 }
 
 const store=redux.createStore(counterReducer);
@@ -14,7 +32,6 @@ const conterSubscriber=()=>{
 
 store.subscribe(conterSubscriber)
 store.dispatch({type:'increment'})
-store.dispatch({type:'increment'})
-store.dispatch({type:'increment'})
-store.dispatch({type:'increment'})
-store.dispatch({type:'increment'})
+store.dispatch({type:'decrement'})
+store.dispatch({type:'incrementby2'})
+store.dispatch({type:'decrementby2'})
